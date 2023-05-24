ba5b82057f0b27f214d265299b44b6e3ebf8d461
leon guilherme <leonlopes1106@gmail.com>
2023-05-24 17:43:26 -0300

    fix: correção no middleware de erro , transformando em função

--- src/middlewares/manipuladorDeErros.js
+++ src/middlewares/manipuladorDeErros.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-class ManipuladorDeErros{
 // eslint-disable-next-line no-unused-vars
-  manipulandoErros(erro, req, res, next){
-    if(erro instanceof mongoose.Error.CastError) {
-      res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos"});
-    }else{
-      res.status(500).send({message: "Error interno do servidor"});
-    }
+function manipulandoErros(erro, req, res, next){
+  if(erro instanceof mongoose.Error.CastError) {
+    res.status(400).send({message: "Um ou mais dados fornecidos estão incorretos"});
+  }else{
+    res.status(500).send({message: "Error interno do servidor"});
   }
 }
 
-export default ManipuladorDeErros;
+
+export default manipulandoErros;
